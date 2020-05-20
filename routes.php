91428b314b604ad8490b280f6f852aeb018b8cf1
ba5nanas <ba5nanas@gmail.com>
2020-05-20 16:27:52 +0700

    Init

---
+++ routes.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: Thecreidea Routes
+ * Plugin URI: https://github.com/Ba5nanas/wp-routes
+ * Description: This's plugin routes help your build website react and vue.js .
+ * Version: 0.1 Beta
+ * Author: Thecreidea
+ * Author URI: http://www.thecreidea.com/
+ */
